9d551c73fa05548a2b7c2d51ff88242bb85e027a
Mr Wait <brennanmitchell1192@gmail.com>
2023-09-21 18:24:48 -0500

    Initial layout of JS.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var charLowercase = "abcdefghijklmnopqrstuvwxyz";
+var charUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charNumbers = 123456789;
+var charSpecial = "!@#$%^&*()"
 
 // Write password to the #password input
 function writePassword() {
@@ -12,4 +16,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+if addEventListener = prompt("How long would you like your password to be? 8-124 characters.");
 
+// Prompt for length of PW 8-124
+
+// Prompt for characters in the password
+// Prompt for including lowercase
+// Prompt for including uppercase
+// Prompt for including special characters
+// Prompt for including numbers
+// Generate PW
+// Display PW
